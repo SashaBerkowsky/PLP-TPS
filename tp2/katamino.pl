35a3457aa989002188fccab1bba49060673b15a1
Sasha Berkowsky <snberkowsky@gmail.com>
2025-06-18 11:39:35 -0300

    hasta ej 11 (TODO ej 7)

--- tp2/katamino.pl
+++ tp2/katamino.pl
@@ -1,68 +1,110 @@
 :- use_module(piezas).
 
-% Completar ...
-%1
-partir(N, Lista, L1, L2) :-
-    append(L1, L2, Lista),
-    length(L1, N).
+% 1
+sublista(D,T,L,R) :- 
+    length(PD, D),
+    length(R, T),
+    append(PD, PR, L),
+    append(R, _, PR).
 
-sublista(D,T,L,R) :- partir(D,L,_,L2), partir(T, L2, R, _).
-
-%2 
+% 2 
 tablero(K, T) :-
     K > 0,
     length(T, 5),
-    maplist(es_fila(K), T).
+    maplist(esFila(K), T).
 
-es_fila(C, F) :-
+esFila(C, F) :-
     length(F, C).
   
-%3
+% 3
 tamaño(M, F, C) :-
     length(M, F),
     F > 0,
     nth0(0, M, F1),
     length(F1, C).
 
-%4
+% 4
 coordenadas(T, IJ) :-
     tamaño(T, X, Y),
     between(1, X, I),
     between(1, Y, J),
     IJ = (I, J).
 
-%5
+% 5
 kPiezas(K, PS) :- nombrePiezas(L), length(PS,K), elegirPiezas(K, L, PS).
 
 elegirPiezas(0, _, []). 
 elegirPiezas(K, [X|L],[X|PS]):- K > 0,K1 is K - 1, elegirPiezas(K1, L, PS).
 elegirPiezas(K, [_|L],PS):- K > 0, elegirPiezas(K, L, PS).
 
-%6 
-buscarFila(T,1, T).
-buscarFila([_|T],FILA, PS):- FILA > 1, FILA1 is FILA - 1, buscarFila(T,FILA1, PS).
+% 6 
+seccionTablero(T, ALTO, ANCHO, (I, J), ST) :-
+    PF is I - 1,
+    PC is J - 1,
 
-ejercicio([], _, _, _, []).
-ejercicio(, 0,, _,[]).
-ejercicio([X|T], ALTO, ANCHO, (_, J), ST) :-
-    J1 is J - 1,
-    sublista(J1, ANCHO, X, R),
-    ALTO1 is ALTO - 1,
-    ejercicio(T, ALTO1, ANCHO, (_, J), ST1),
-    append(R, ST1, ST).
-seccionTablero(T,ALTO, ANCHO, IJ, ST):- IJ = (I,_), buscarFila(T,I, T1), ejercicio(T1, ALTO, ANCHO, IJ, ST).
+    tamaño(T, FT, CT),
 
-%7
-ubicarPieza(T, Id):- pieza(Id,F), ubicar(T,F,T).
+    FF is PF + ALTO,
+    FF =< FT,
+    FC is PC + ANCHO,
+    FC =< CT,
 
-ubicar(T,[],T).
-ubicar([X|T],[Y|F], [Z|T1]):- poner(X,Y,Z), ubicar(T,F,T1).
-ubicar([X|T],F, T):- ubicar(T,F,T1).
+    sublista(PF, ALTO, T, FS),
 
-poner(X,[],X). 
-poner([B|X],[A|Y],[A|Z]):- var(B), poner(X,Y,Z). 
-%8
+    maplist(obtenerCols(PC, ANCHO), FS, ST).
+
+obtenerCols(J, ANCHO, FS, FA) :-
+    sublista(J, ANCHO, FS, FA).    
+
+% 7
+ubicarPieza(Tablero, Identificador) :-
+    pieza(Identificador, FormaPieza), 
+    tamaño(FormaPieza, AltoPieza, AnchoPieza),
+    tamaño(Tablero, FilasTablero, ColsTablero),
+
+    between(1, FilasTablero, I),
+    between(1, ColsTablero, J),
+
+    seccionTablero(Tablero, AltoPieza, AnchoPieza, (I, J), SeccionTablero),
+
+    colocar_pieza_en_seccion(Identificador, FormaPieza, SeccionTablero).
+
+colocar_pieza_en_seccion(_, [], []). 
+colocar_pieza_en_seccion(Identificador, [FilaPieza | RestoPieza], [FilaTablero | RestoTablero]) :-
+    colocar_fila_pieza(Identificador, FilaPieza, FilaTablero),
+    colocar_pieza_en_seccion(Identificador, RestoPieza, RestoTablero).
+
+colocar_fila_pieza(_, [], []). 
+colocar_fila_pieza(Identificador, [P_Celda | P_Resto], [T_Celda | T_Resto]) :-
+    (   P_Celda == Identificador
+    ->  
+        var(T_Celda),
+        T_Celda = Identificador
+    ;   var(P_Celda) 
+    ->  
+        true
+    ;   false 
+    ),
+    colocar_fila_pieza(Identificador, P_Resto, T_Resto).
+
+% 8
 ubicarPiezas(_, _, []).
-ubicarPiezas(T0, Poda, [X|IdL]) :- 
-    ubicarPieza(T0, X, T1), 
-    ubicarPiezas(T1, Poda, IdL).
+ubicarPiezas(T, Poda, [PA|PR]) :-
+    ubicarPieza(T, PA),
+    poda(Poda, T),
+    ubicarPiezas(T, Poda, PR).
+
+% 9
+llenarTablero(Poda, C, T) :-
+    tablero(C, T),
+    kPiezas(C, P),
+    ubicarPiezas(T, Poda, P).
+
+% 10
+cantSoluciones(Poda, Columnas, N) :-
+    findall(T, llenarTablero(Poda, Columnas, T), TS),
+    length(TS, N).
+
+% 11
+poda(sinPoda, _) :- true.
+
